a2f432ebeb77da7289f9221ecb5e57b39ebb2ae0
Cascadia-PawanjitNayyar <pawanjitknayyar@cc1-230-401.student.cascadia.edu>
2015-05-29 17:35:07 -0700

    updated the code for view/edit
    updated code

--- MyContactList/FirstViewController.swift
+++ MyContactList/FirstViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var scrollview: UIScrollView!
     
+    @IBOutlet weak var btnChange: UIButton!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtHomePhone: UITextField!
+    @IBOutlet weak var txtCell: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +31,22 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeEditMode(sender: AnyObject) {
+        let controls = [txtName,txtAddress,txtCity,txtState,txtZip,txtCell,txtHomePhone,txtEmail]
+        
+        for control in controls{
+            if (sender.selectedSegmentIndex == 0) {
+                control.enabled=false
+                control.borderStyle = UITextBorderStyle.None
+                btnChange.enabled = false
+                
+            } else {
+                control.enabled=true
+                control.borderStyle = UITextBorderStyle.RoundedRect
+                btnChange.enabled = true
+            }
+        }
+    }
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
 }
