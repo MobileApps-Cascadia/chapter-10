e1b3b4ed88abe542d8535173d623eb1674d18bbd
Zin Maung <zindoublem@gmail.com>
2017-05-11 23:57:58 -0700

    Add Segmented Controller, Navigation Controller, ScrollView, and Contact UI elements

--- MyContactList/ContactViewController.swift
+++ MyContactList/ContactViewController.swift
@@ -11,22 +11,41 @@ import UIKit
 class ContactViewController: UIViewController {
 
     @IBOutlet weak var myStack: UIStackView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtCell: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var lblBirthdate: UILabel!
+    @IBOutlet weak var btnChange: UIButton!
     
     
+    // set the scrollview content size and initialize the edit mode
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
         
-        // Add a Center Anchor Constraint myStack.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active=true;
-        
-        
-    }
+        // Add a Center Anchor Constraint
+        myStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true;
+        //scrollView.contentSize = CGSize(320, 500);
 
+       
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Change the borderStyle and enabled attributes for all the txt elements based on
+    // whether the user is editing the contact.
+    @IBAction func changeEditMode(sender: AnyObject) {
+        _ = [txtName, txtAddress, txtCity, txtState, txtZip, txtCell, txtPhone, txtEmail]
+    }
 }
 
